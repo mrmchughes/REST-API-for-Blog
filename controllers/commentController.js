5be935f0945d8255bd7579bd97ddd47100cadf0d
Michael <mrmchughes27@gmail.com>
2022-08-29 16:23:44 -0400

    worked on comment controller

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -26,38 +26,54 @@ exports.create_comment = function (req, res, next) {
     message: req.body.message,
   });
 
-  const { postId } = req.params;
-  const post = Post.findById(postId);
-
-  post.update({ _id: post._id }, { $push: { comments: comment } });
+  Post.findByIdAndUpdate(req.params.postId, {
+    $push: { comments: { comment } },
+  });
 };
 
 exports.get_comments = function (req, res, next) {
-  Comment.find({}).exec(function (err, list_comments) {
-    if (err) {
-      return next(err);
+  async.parallel(
+    {
+      post(callback) {
+        Post.findById(req.params.postId).exec(callback);
+      },
+    },
+
+    (err, results) => {
+      if (err) {
+        return next(err);
+      }
+      if (results.post.comments == null) {
+        const err = new Error("Post comments not found");
+        err.status = 404;
+        return next(err);
+      }
+      res.send(results.post.comments);
     }
-    res.send(list_comments);
-  });
+  );
 };
 
 exports.get_comment = function (req, res, next) {
   async.parallel(
     {
-      comment(callback) {
+      post(callback) {
+        Post.findById(req.params.postId).exec(callback);
+      },
+      comments(callback) {
         Comment.findById(req.params.commentId).exec(callback);
       },
     },
+
     (err, results) => {
       if (err) {
         return next(err);
       }
-      if (results.post == null) {
+      if (results.post.comments.comment == null) {
         const err = new Error("Comment not found");
         err.status = 404;
         return next(err);
       }
-      res.send(results.comment);
+      res.send(results.post.comments.comment);
     }
   );
 };
