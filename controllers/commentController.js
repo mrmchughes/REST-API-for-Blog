dcad4954ce05f92fc06a7cca5d26533a5909b3b2
Michael <mrmchughes27@gmail.com>
2022-10-13 14:40:24 -0400

    adjusted comment timestamp

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -91,13 +91,10 @@ exports.update_comment = function (req, res, next) {
     .withMessage("Comment message must be specified.");
 
   let currentDate = new Date();
-  let time =
-    currentDate.getHours() +
-    ":" +
-    currentDate.getMinutes() +
-    ":" +
-    currentDate.getSeconds();
-  let organizedDate = currentDate.toLocaleDateString();
+  let time = currentDate.getHours() + ":" + currentDate.getMinutes();
+
+  let options = { month: "short", day: "numeric", year: "numeric" };
+  let organizedDate = currentDate.toLocaleDateString("en-US", options);
 
   Comment.findByIdAndUpdate(
     req.params.commentId,
